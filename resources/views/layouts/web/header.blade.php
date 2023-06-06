f272c8bb06b9f21dfd0a86b66104109c65284c68
haongo2007 <haongodev@gmail.com>
2023-06-06 17:13:35 +0700

    Page A001 TOP

---
+++ resources/views/layouts/web/header.blade.php
@@ -0,0 +1,12 @@
+<div class="main-header">
+    <div class="left-side">
+        <div class="main-logo">
+            <img src="{{ asset('assets') }}/images/logo/title_logo.png" alt="training_management_logo">
+        </div>
+    </div>
+    <div class="right-side">
+        <div class="container">
+            <button type="button" class="header-buttom">私の研鑽データ</button>
+        </div>
+    </div>
+</div>
