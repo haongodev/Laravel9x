7d32b0137fbf11ea0ef19c2280fe4b45b57906db
Anh Tran <anh.tdt>
2023-08-30 10:18:48 +0700

    Plative 85

--- resources/views/myPage/creditRegistration/search_type_selected.blade.php
+++ resources/views/myPage/creditRegistration/search_type_selected.blade.php
@@ -3,6 +3,7 @@
        class="registered"
        data-answer-manage-id="{{$credits->answer_manage_id}}"
        data-original-question-id="{{$credits->original_question_id}}"
+       data-type-native-id = {{$typeNativeId}}
     >
         @php
             $arrAnswer =explode(',',$credits->answer2 ?? '');
@@ -17,13 +18,14 @@
         $('.registered').click(function () {
             var answer_manage_id = $(this).data('answer-manage-id');
             var original_question_id = $(this).data('original-question-id');
-            console.log(answer_manage_id,original_question_id)
+            var type_native_id = $(this).data('type-native-id');
+
             $.ajax({
                 type: "post",
                 url: '{{route('popupRegistered')}}',
                 cache: false,
                 headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                data: {answer_manage_id: answer_manage_id, original_question_id:original_question_id},
+                data: {answer_manage_id: answer_manage_id, original_question_id:original_question_id, type_native_id :type_native_id},
                 success: function (data) {
                     console.log(data);
                     $('body').append(data);
