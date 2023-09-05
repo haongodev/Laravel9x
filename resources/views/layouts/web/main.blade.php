1eeaacba894e9e0ad3c715f3736a3d130c9e8e51
haongodev <haongodev@gmail.com>
2023-09-05 11:14:52 +0700

    fix Plative 107

--- resources/views/layouts/web/main.blade.php
+++ resources/views/layouts/web/main.blade.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>{{ isset($pageSlug) ? $pageSlug : config('app.name', 'Training Management') }}</title>
+    {{-- <title>{{ isset($pageSlug) ? $pageSlug : config('app.name', 'Training Management') }}</title> --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    {{-- <title>{{ (isset($page)) ? $page : config('app.name', 'Training Management') }}</title> --}}
     <title>研鑽管理システム</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0 , user-scalable=0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
