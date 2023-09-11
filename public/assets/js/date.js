bc1e7be3ef2e12d3cb2659db7490aac8e44138d7
Anh Tran <anh.tdt>
2023-09-11 14:28:52 +0700

    feedback 94

--- public/assets/js/date.js
+++ public/assets/js/date.js
@@ -2,10 +2,7 @@ $.datepicker.setDefaults($.datepicker.regional['jp']);
 $( ".datepicker" ).datepicker({
     dateFormat: "yy-mm-dd",
     showMonthAfterYear:true,
-    beforeShow: function (elem, inst){
-        console.log($('#ui-datepicker-div').find('.ui-datepicker-year').html());
-        $('#ui-datepicker-div').find('.ui-datepicker-year').append('axx');
-    }
+
 });
 
 $('.date-icon').on('click', function() {
