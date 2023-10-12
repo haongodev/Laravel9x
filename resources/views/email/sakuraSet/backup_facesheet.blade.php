796b3d3a29edf970c8ccc3f142ce97b348c444ca
haongodev <haongodev@gmail.com>
2023-10-12 12:27:51 +0700

    update email template for teach sent email

--- resources/views/email/sakuraSet/backup_facesheet.blade.php
+++ resources/views/email/sakuraSet/backup_facesheet.blade.php
@@ -1,14 +1,10 @@
-{{-- <div>
+<div>
     <p>構成員番号: {{ $config['sakuraData']->made_member->login_id }}</p>
     <p>{{ $config['sakuraData']->made_member->name1.' '.$config['sakuraData']->made_member->name2 }} 様</p>
-    <p>生涯研修制度「研鑽管理システム」よりお知らせです。    </p>
-    <p>{{ $config['sakuraData']->reviewer_member->name1.' '.$config['sakuraData']->reviewer_member->name2 }} 様（構成員番号{{ $config['sakuraData']->reviewer_member->login_id }}）がフェイスシートを共有しました。</p>
+    <p>生涯研修制度「研鑽管理システム」よりお知らせです。</p>
+    <p>{{ $config['sakuraData']->reviewer_member->name1.' '.$config['sakuraData']->reviewer_member->name2 }} 様（構成員番号{{ $config['sakuraData']->reviewer_member->login_id }}）が振り返りシートを更新しました。</p>
     <p>以下の＜ご確認の流れ＞を参考に、ご確認のほど宜しくお願い申しあげます。    </p>
     <p>＜ご確認の流れ＞</p>
     <p>構成員マイページにログイン→「私の研鑽データ」にアクセス→「自己研鑽支援ツール『さくらセット』に取り組む」を確認    </p>
 @include('email.sakuraSet.emailTemplateFooter')
-</div> --}}
-<div>
-    <p>振返り担当者（{{ $config['sakuraData']->reviewer_member->name1.' '.$config['sakuraData']->reviewer_member->name2 }}※）がファイスシートを修正しました。</p>
-    <p>研修システムにログインし、さくらセットに取り組むを確認してください。</p>
 </div>
